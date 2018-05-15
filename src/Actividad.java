b71d577098195209441770047cf1370ea4410228
Trivinox <39246670+Trivinox@users.noreply.github.com>
2018-05-15 16:19:10 -0500

    Update Actividad.java

--- src/Actividad.java
+++ src/Actividad.java
@@ -1,4 +1,29 @@
 
 public class Actividad {
+	String titulo;
+	int calificacion;
 
+	public Actividad(String titulo, int calificacion) {
+		super();
+		this.titulo = titulo;
+		this.calificacion = calificacion;
+	}
+
+	/*
+	 * Prueba si una actividad fue calificada con una nota superior a otra
+	 */
+	boolean esMejorQue(Actividad otraActividad) {
+		if (this.calificacion > otraActividad.calificacion) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/*
+	 * Imprime la actividad requerida
+	 */
+	void mostrarActividad() {
+		System.out.println("Actividad: " + this.titulo);
+	}
 }
