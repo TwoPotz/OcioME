74c235efc7bcb4b4d2384bea346a7ccc68a12182
Trivinox <39246670+Trivinox@users.noreply.github.com>
2018-05-15 16:20:21 -0500

    Update Ocio.java

--- src/Ocio.java
+++ src/Ocio.java
@@ -1,4 +1,21 @@
-
 public class Ocio {
+	private String tipo;
+	private int cantidadActividades;
+	Actividad[] actividades = new Actividad[cantidadActividades];
 
+	
+	
+	public Ocio(String tipo, int cantidadActividades, Actividad[] actividades) {
+		super();
+		this.tipo = tipo;
+		this.cantidadActividades = cantidadActividades;
+		this.actividades = actividades;
+	}
+	void verListasDeActividades() {
+		System.out.println("[#] Actividad");
+		for (int i = 0; i <= cantidadActividades; i++) {
+			System.out.println("[" + i+1 + "] " + this.actividades[i]);
+		}
+	}
+	
 }
