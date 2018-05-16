fc7b68b9518dbe46c67f95a68a4116c14acc9f04
DanielUribeVillada <39224712+DanielUribeVillada@users.noreply.github.com>
2018-05-15 22:37:22 -0500

    Update Estudiante.java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Estudiante {
+
+String nombre;
+String carrera;
+int edad;
+int cantidadOcios;
+Ocio []ocios;
+
+
+private static void saludar(){
+ 		imprimir("Bienvenidos al programa de OcioME");
+ 		}
+
+public void mostrarPerfil() {
+	String txt="Nombre "+this.nombre+"\n"+"Carrera: "+this.carrera+"\n"+"Edad: "+this.edad+"\n"+"Cantidad de ocios: "+this.cantidadOcios+"\n";
+	System.out.println(txt);
+	   }
+
+private static void imprimir(String txt){
+	//System.out.println(txt);
+ 		JOptionPane.showMessageDialog(null,txt);
+ 	}
 
 }
