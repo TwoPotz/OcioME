ffa50c3ea47b7a6eb4316c5bfa41b7d59d38c49e
DanielUribeVillada <39224712+DanielUribeVillada@users.noreply.github.com>
2018-05-15 22:46:13 -0500

    Update Estudiante.java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -10,16 +10,28 @@ int edad;
 int cantidadOcios;
 Ocio []ocios;
 
-
+/**
+ * Muestra un breve saludo al usario.
+ */
 private static void saludar(){
  		imprimir("Bienvenidos al programa de OcioME");
  		}
 
+/**
+ * Muestra el perfil de los usarios registrados con OcioME.
+ */
 public void mostrarPerfil() {
 	String txt="Nombre "+this.nombre+"\n"+"Carrera: "+this.carrera+"\n"+"Edad: "+this.edad+"\n"+"Cantidad de ocios: "+this.cantidadOcios+"\n";
 	System.out.println(txt);
 	   }
 
+public void verOcios(String nombre) {
+	
+}
+
+/**
+ * Imprime la informacion.
+ */
 private static void imprimir(String txt){
 	//System.out.println(txt);
  		JOptionPane.showMessageDialog(null,txt);
