2b6939f46959b6f296b0a28edfbf230042e160e1
DanielUribeVillada <39224712+DanielUribeVillada@users.noreply.github.com>
2018-05-17 20:22:12 -0500

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,26 @@
+import javax.swing.JOptionPane;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		saludar();
+		
 
 	}
 
+	
+/**
+* Muestra un breve saludo al usario.
+*/
+	private static void saludar(){
+		imprimir("Bienvenidos al programa de OcioME");
+		}
+	
+/**
+* Imprime la informacion.
+*/
+	private static void imprimir(String txt){
+		//System.out.println(txt);
+	 		JOptionPane.showMessageDialog(null,txt);
+	 	}
 }
