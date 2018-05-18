10c22d465707b746c41c59018403a9b274c6cae0
TwoPotz <39315655+TwoPotz@users.noreply.github.com>
2018-05-18 13:29:18 -0500

    Update Estudiante.java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -4,37 +4,72 @@ import javax.swing.JOptionPane;
 
 public class Estudiante {
 
-String nombre;
-String carrera;
-int edad;
-int cantidadOcios;
-Ocio []ocios;
+	String nombre;
+	String carrera;
+	int edad;
+	int compararEdades;
+	int cantidadOcios;
+	Ocio[] ocios;
 
-/**
- * Muestra un breve saludo al usario.
- */
-private static void saludar(){
- 		imprimir("Bienvenidos al programa de OcioME");
- 		}
+	/**
+	 * Muestra un breve saludo al usario.
+	 */
+	private static void saludar() {
+		imprimir("Bienvenidos al programa de OcioME");
+	}
 
-/**
- * Muestra el perfil de los usarios registrados con OcioME.
- */
-public void mostrarPerfil() {
-	String txt="Nombre "+this.nombre+"\n"+"Carrera: "+this.carrera+"\n"+"Edad: "+this.edad+"\n"+"Cantidad de ocios: "+this.cantidadOcios+"\n";
-	System.out.println(txt);
-	   }
+	/**
+	 * Muestra el perfil de los usarios registrados con OcioME.
+	 */
+	public void mostrarPerfil() {
+		String txt = "Nombre " + this.nombre + "\n" + "Carrera: " + this.carrera + "\n" + "Edad: " + this.edad + "\n"
+				+ "Cantidad de ocios: " + this.cantidadOcios + "\n";
+		System.out.println(txt);
+	}
 
-public void verOcios(String nombre) {
+	public void verOcios(String nombre) {
+
+	}
+
+	/**
+	 * Imprime la informacion.
+	 */
+	private static void imprimir(String txt) {
+		// System.out.println(txt);
+		JOptionPane.showMessageDialog(null, txt);
+	}
+
+public static void compararEstudiantes(String txt) {
+	String terror;
+	String ocio1=terror;
+	String ocio2=terror;
+	ocio1=JOptionPane.showMessageDialog(null, txt);
+	ocio2=JOptionPane.showMessageDialog(null, txt);
+	
+	if(ocio1.equals(ocio2)) {
+		JOptionPane.showMessageDialog(null, "Comparten los mismos ocios");	
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "tienen diferentes ocios");
+	}
 	
 }
 
-/**
- * Imprime la informacion.
- */
-private static void imprimir(String txt){
-	//System.out.println(txt);
- 		JOptionPane.showMessageDialog(null,txt);
- 	}
+	public void compararGustos() {
+
+	}
+
+	public void preguntarOcio() {
+
+	}
+
+	public void aniadirOcio(String titulo) {
+
+	}
+
+	public static boolean compararCarreras() {
+		return false;
+
+	}
 
 }
